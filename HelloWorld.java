7733078518bb2a93fe6c5eeee0b21e2e73482d02
Thomas <tguyon14@gmail.com>
2025-04-18 10:38:49 +0200

    English comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
     public static void main(String[] args) {
         
-        //Affichage de texte à l'ecran
+        //Displaying text on the screen
         System.out.println("Hello World !");
     }
 }
